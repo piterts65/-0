57e7f52febe28881f762f6314c37f459163d4ca0
piterts65 <piterts65@gmail.com>
2024-08-13 22:33:46 +0500

    Add files via upload

---
+++ mod1-1.py
@@ -0,0 +1,6 @@
+a ='Крокодил'
+print(a[0])
+print(a[-1])
+print(a[5:])
+print(a[::-1])
+print(a[1:8:2])
---
+++ mod1-2.py
@@ -0,0 +1,5 @@
+home_work=12
+hoir=1.5
+name="Pithon"
+time_on_one=hoir/home_work
+print('Курс:',name,'всего задач:',home_work,'затрачено часов:',hoir,'среднее время выполнения',time_on_one,'часа.')
