ee2b34a97508db1ddfd3ce6c2387d790b446139e
piterts65 <piterts65@gmail.com>
2024-08-19 09:27:12 +0500

    Add files via upload

---
+++ modul_2_3.py
@@ -0,0 +1,28 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+print('Исходные данные:',my_list)
+print('Вывод на кoнсоль:')
+index = 0
+while index<len(my_list):
+    if my_list[index] > 0:
+        print(my_list[index])
+    if my_list[index]<0:
+        break
+    index += 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
