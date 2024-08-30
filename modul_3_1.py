253835647f37fd05ee35bf5f6c533bc94644ff13
piterts65 <piterts65@gmail.com>
2024-08-30 16:26:00 +0500

    Add files via upload

---
+++ modul_3_1.py
@@ -0,0 +1,43 @@
+calls = 0
+def count_calls():
+    global calls
+    calls += 1
+def string_info(string):
+    count_calls()
+    return len(string), string.upper(), string.lower()
+def is_contains(string,list_to_search):
+    count_calls()
+    string = string.lower()
+    for i in list_to_search:
+        if i.lower() == string:
+            return True
+            return False
+        
+print(string_info('CopyBARA'))
+print(string_info('armfgeDon'))
+print(string_info('TicHer'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))# Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic']))# No matches
+print(is_contains('Glass',['Klass','шанс','glasS','pass']))# Glass ~ glasS
+print(calls)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
